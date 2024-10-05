b1cfe242867eff81e21e808ebf00c8707d6b2e9f
veritas <veritas9872@gmail.com>
2024-10-05 18:14:14 +0900

    Implement functionality.

---
+++ matmul/bf16.py
@@ -0,0 +1,77 @@
+"""
+Run `python -m fire perf/matmul_bf16.py measure` to run for the provided shapes.
+Run `python -m fire perf/matmul_bf16.py prof_matmul $m $k $n` for arbitrary shapes.
+"""
+from statistics import mean, stdev
+
+import torch
+from torch import nn, Tensor
+import habana_frameworks.torch.hpu as ht
+
+
+@torch.inference_mode()
+def prof_matmul(
+        m: int,
+        k: int,
+        n: int,
+        num_steps: int = 256,
+        data_type: str = "bf16",
+) -> None:
+    if data_type == "fp32":
+        dtype = torch.float32
+    elif data_type == "bf16":
+        dtype = torch.bfloat16
+    else:
+        raise ValueError(f"Invalid data type {data_type}.")
+
+    a = torch.randn(size=(m, k), device="hpu", dtype=dtype)
+    w = torch.randn(size=(n, k), device="hpu", dtype=dtype)
+
+    class MM(nn.Module):
+        def __init__(self, mat1: Tensor, mat2: Tensor):
+            super().__init__()
+            self.m1 = mat1
+            self.m2 = mat2
+
+        def forward(self):
+            return torch.mm(input=self.m1, mat2=self.m2)
+
+    mm = ht.wrap_in_hpu_graph(MM(mat1=a, mat2=w.T))
+    for _ in range(16):  # Warmup
+        mm()
+
+    tics = [ht.Event(enable_timing=True) for _ in range(num_steps)]
+    tocs = [ht.Event(enable_timing=True) for _ in range(num_steps)]
+
+    for i in range(num_steps):
+        tics[i].wait()  # Wait until everything in front has finished.
+        tics[i].record()
+        mm()
+        tocs[i].record()
+
+    ht.synchronize()  # Synchronize after finishing.
+    mss = [tic.elapsed_time(toc) for tic, toc in zip(tics, tocs, strict=True)]
+    # Using the exact TFLOPS equation.
+    tfps = [1e-9 * m * n * (2 * k - 1) / ms for ms in mss]
+    print(
+        f"({m:6}x{k:6})x({k:6}x{n:6}): 
+        Mean {mean(tfps):5.1f} TFLOPS, 
+        Min {min(tfps):5.1f} TFLOPS, 
+        Max {max(tfps):5.1f} TFLOPS, 
+        STDEV {stdev(tfps):7.3f} TFLOPS"
+    )
+
+
+def measure(num_steps: int = 256, data_type: str = "bf16") -> None:
+    mkn = (
+        (16384, 8192, 1280),
+        (16384, 1024, 8192),
+        (16384, 8192, 7168),
+        (16384, 3584, 8192),
+        (2 ** 12, 2 ** 12, 2 ** 12),
+        (2 ** 13, 2 ** 13, 2 ** 13),
+        (2 ** 14, 2 ** 14, 2 ** 14),
+        (2 ** 15, 2 ** 15, 2 ** 15),
+    )
+    for m, k, n in mkn:
+        prof_matmul(m, k, n, num_steps=num_steps, data_type=data_type)
---
+++ matmul/fp8.py
@@ -0,0 +1,119 @@
+"""
+Available scaling methods can be found in the following link.
+https://docs.habana.ai/en/latest/PyTorch/Inference_on_PyTorch/Inference_Using_FP8.html#supported-json-config-file-options
+
+export PT_HPU_WEIGHT_SHARING=0
+export LOG_LEVEL_HQT=1
+export PT_HPU_LAZY_MODE=1
+
+Run `python -m fire perf/matmul_bf16.py measure` to run for the provided shapes.
+Run `python -m fire perf/matmul_bf16.py prof_matmul $m $k $n` for arbitrary shapes.
+"""
+from statistics import mean, stdev
+
+import torch
+from torch import nn
+import habana_frameworks.torch as ht
+import habana_frameworks.torch.core as htcore
+from neural_compressor.torch.quantization import (
+    FP8Config, 
+    convert, 
+    prepare, 
+    finalize_calibration,
+)
+
+
+htcore.hpu_set_env()
+
+
+@torch.inference_mode()
+def prof_matmul(
+        m: int,
+        k: int,
+        n: int,
+        num_steps: int = 256,
+        fp8_config: str = "E4M3",
+        scale_method: str = "maxabs_hw",
+        measure_mode: bool = True,
+):
+    config_measure = FP8Config.from_dict({
+        "fp8_config": fp8_config,
+        "mode": "MEASURE",
+        "observer": "maxabs",
+        "allowlist": {"types": [], "names":  []},
+        "blocklist": {"types": [], "names":  []},
+        "dump_stats_path": f"./inc_output/{fp8_config}_{scale_method}_{m}_{k}_{n}",
+    })
+
+    config_quantize = FP8Config.from_dict({
+        "fp8_config": fp8_config,
+        "mode": "QUANTIZE",
+        "observer": "maxabs",
+        "scale_method": scale_method,
+        "allowlist": {"types": [], "names":  []},
+        "blocklist": {"types": [], "names":  []},
+        "dump_stats_path": f"./inc_output/{fp8_config}_{scale_method}_{m}_{k}_{n}"
+    })
+
+    print("Initializing.")
+    a = torch.randn(size=(m, k), device="hpu")
+    # The Sequential layer is necessary because INC cannot take a single module.
+    model = nn.Sequential(nn.Linear(in_features=k, out_features=n, bias=False, device="hpu"))
+
+    if measure_mode:
+        model_measure = prepare(model, config_measure)
+        htcore.hpu_inference_initialize(model_measure, mark_only_scales_as_const=True)
+        print("Starting measurement.")
+
+        for _ in range(16):
+            model_measure(a)
+
+        finalize_calibration(model_measure)
+        print("Finishing measurement.")
+    else:
+        model_quant = convert(model, config_quantize)
+        htcore.hpu_inference_initialize(model_quant, mark_only_scales_as_const=True)
+        model_quant = ht.hpu.wrap_in_hpu_graph(model_quant)
+
+        print("Starting quantized run.")
+
+        for _ in range(16):  # Warmup
+            model_quant(a)
+        
+        tics = [ht.hpu.Event(enable_timing=True) for _ in range(num_steps)]
+        tocs = [ht.hpu.Event(enable_timing=True) for _ in range(num_steps)]
+        for i in range(num_steps):
+            tics[i].wait()  # Prevent asynchronous launches.
+            tics[i].record()
+            model_quant(a)
+            tocs[i].record()
+        ht.hpu.synchronize()
+
+        mss = [tic.elapsed_time(toc) for tic, toc in zip(tics, tocs, strict=True)]
+        # Using the exact matmul flops equation.
+        tflops = [1e-9 * m * n * (2 * k - 1) / ms for ms in mss]
+
+        print(
+            f"{fp8_config} {scale_method} scaling matmul ({m}x{k})x({k}x{n}): 
+            Mean {mean(tflops):.2f} TFLOPS, 
+            Min: {min(tflops):.2f} TFLOPS, 
+            Max: {max(tflops):.2f} TFLOPS, 
+            STDEV: {stdev(tflops):.2f} TFLOPS"
+        )
+
+
+def measure(num_steps: int = 256, fp8_config: str = "E4M3", scale_method: str = "maxabs_hw") -> None:
+    mkn = (
+        (16384, 8192, 1280),
+        (16384, 1024, 8192),
+        (16384, 8192, 7168),
+        (16384, 3584, 8192),
+        (2 ** 12, 2 ** 12, 2 ** 12),
+        (2 ** 13, 2 ** 13, 2 ** 13),
+        (2 ** 14, 2 ** 14, 2 ** 14),
+        (2 ** 15, 2 ** 15, 2 ** 15),
+    )
+    kwargs = dict(num_steps=num_steps, fp8_config=fp8_config, scale_method=scale_method)
+    for m, k, n in mkn:
+        prof_matmul(m, k, n, measure_mode=True, **kwargs)
+        prof_matmul(m, k, n, measure_mode=False **kwargs)
