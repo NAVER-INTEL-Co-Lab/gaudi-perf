f582985c002c10118db1032d481aeca42e4674ef
Joonhyung Lee/이준형 <33523965+veritas9872@users.noreply.github.com>
2024-11-07 18:52:38 +0900

    Dev/fp8 (#10)
    * Enable static scaling in FP8 matrix multiplication.
    
    * Minor updates to Llama run configurations.
    
    * Implement prefill throughput measurements for Llama in FP8.
    
    * Fix spacing in docs.
    
    ---------
    
    Co-authored-by: joonhyung.lee <joonhyung.lee@navercorp.com>

--- prefill/llama_fp8.py
+++ prefill/llama_fp8.py
@@ -2,14 +2,29 @@
 Login to HuggingFace with `huggingface-cli login` if a gated repo is to be used.
 
 Example run command below.
+
 ```bash
+export PT_HPU_WEIGHT_SHARING=0
+export LOG_LEVEL_HQT=1
+export PT_HPU_LAZY_MODE=1
+
+# First run with `measure_mode` enabled to get quantization statistics.
 deepspeed --no_local_rank --num_gpus 8 \
-    --module fire prefill/llama.py main \
+    --module fire prefill/llama_fp8.py main \
     --model_name meta-llama/Llama-3.1-70B \
     --seq_len $((8 * 1024)) \
-    --num_steps 32
+    --num_steps 32 \
+    --measure_mode True
+
+# Then run with `measure_mode` disabled for the actual run.
+deepspeed --no_local_rank --num_gpus 8 \
+    --module fire prefill/llama_fp8.py main \
+    --model_name meta-llama/Llama-3.1-70B \
+    --seq_len $((8 * 1024)) \
+    --num_steps 32 \
+    --measure_mode False
 ```
-    
+
 For power measurements, use one of the following commands on the host.
 The container will likely not have `ipmitool` available.
 
@@ -24,15 +39,18 @@ from statistics import mean, stdev
 
 import torch
 import torch.distributed as dist
-import deepspeed
+import habana_frameworks.torch.core as htcore
 import habana_frameworks.torch.hpu as ht
 import habana_frameworks.torch.distributed.hccl  # noqa
 from optimum.habana.transformers.modeling_utils import adapt_transformers_to_gaudi
 from optimum.habana.utils import get_habana_frameworks_version
-from habana_frameworks.torch.hpu import Event
+
+import deepspeed
+from habana_frameworks.torch.hpu import Event  # noqa
 from transformers import AutoConfig, AutoModelForCausalLM
 
 
+htcore.hpu_set_env()
 adapt_transformers_to_gaudi()
 
 
@@ -148,6 +166,10 @@ def main(
         num_steps: int = 16,
         batch_size: int = 1,
         seq_len: int = 4096,
+        fp8_config: str = "E4M3",
+        scale_method: str = "maxabs_hw",
+        scale_format: str = "const",
+        measure_mode: bool = True,
 ):
     deepspeed.init_distributed(dist_backend="hccl")
     local_rank = int(os.getenv("LOCAL_RANK", "0"))
@@ -155,33 +177,90 @@ def main(
 
     config = AutoConfig.from_pretrained(model_name, torch_dtype=torch.bfloat16)
 
-    dsd = "hpu" if world_size == 1 else "meta"
+    dsd = f"hpu" if world_size == 1 else "meta"
     with deepspeed.OnDevice(dtype=torch.bfloat16, device=dsd):
         model = AutoModelForCausalLM.from_config(config, torch_dtype=torch.bfloat16)
         model.eval()
 
-    hpu_graph = world_size == 1
+    # Hack to prevent OOM on 16K sequence length for 34B.
+    use_hpu_graph = (world_size == 1) and (seq_len < 16384)
+
+    # Checkpoints must be loaded by deepspeed during inference initialization
+    # to prevent entire model weights from being loaded by all processes.
+    # This way, model initialization does not cause a memory spike.
     model = deepspeed.init_inference(
         model,
         dtype=torch.bfloat16,
         tensor_parallel={"tp_size": world_size},
-        enable_cuda_graph=hpu_graph,
-        set_empty_params=True,  # Initialize empty parameters from meta-tensors.
+        enable_cuda_graph=use_hpu_graph,
+        max_out_tokens=1,
+        min_out_tokens=1,
     )
 
-    info = measure(
-        model=model,
-        model_name=model_name,
-        batch_size=batch_size,
-        seq_len=seq_len,
-        num_steps=num_steps,
+    # Imports of these modules must come after DeepSpeed setup.
+    from neural_compressor.torch.quantization import (
+        FP8Config,
+        convert,
+        prepare,
+        finalize_calibration,
     )
-    info.update({
-        "Local Rank": local_rank, 
-        "World Size (TP Degree)": world_size, 
-        "Use HPU Graph": hpu_graph
+    dump_stats_path = f"./inc_output/measure_{model_name}_{seq_len}_{fp8_config}"
+    blocklist_names = ["lm_head", "fused_scaled_dot_product_attention"]
+    config_measure = FP8Config.from_dict({
+        "fp8_config": fp8_config,
+        "mode": "MEASURE",
+        "observer": "maxabs",
+        "allowlist": {"types": [], "names": []},
+        "blocklist": {"types": [], "names": blocklist_names},
+        "dump_stats_path": dump_stats_path,
     })
-    dist.destroy_process_group()
-    if local_rank == 0:  # Only show the results from the main process.
-        pprint(info)
-        print(f"\n\nMean TFLOPS/HPU: {info['Model Mean TFLOPS'] / world_size:.1f} TFLOPS\n\n")
+
+    config_quantize = FP8Config.from_dict({
+        "fp8_config": fp8_config,
+        "mode": "QUANTIZE",
+        "observer": "maxabs",
+        "scale_method": scale_method,
+        "scale_format": scale_format,
+        "allowlist": {"types": [], "names": []},
+        "blocklist": {"types": [], "names": blocklist_names},
+        "dump_stats_path": dump_stats_path,
+    })
+
+    if measure_mode:
+        model_measure = prepare(model.module, config_measure)
+        htcore.hpu_inference_initialize(model_measure, mark_only_scales_as_const=True)
+
+        for _ in range(8):
+            x = torch.randint(
+                low=0,
+                high=1024,
+                size=(batch_size, seq_len),
+                dtype=torch.int64,
+                device="hpu",
+            )
+            model_measure(x)
+        finalize_calibration(model_measure)
+
+    else:
+        model_quant = convert(model.module, config_quantize)
+        htcore.hpu_inference_initialize(model_quant, mark_only_scales_as_const=True)
+
+        info = measure(
+            model=model_quant,
+            model_name=model_name,
+            batch_size=batch_size,
+            seq_len=seq_len,
+            num_steps=num_steps,
+        )
+        info.update({
+            "Local Rank": local_rank,
+            "World Size (TP Degree)": world_size,
+            "Use HPU Graph": use_hpu_graph,
+            "Binary Type": fp8_config,
+            "Scale Method": scale_method,
+            "Scale Format": scale_format,
+        })
+        dist.destroy_process_group()
+        if local_rank == 0:  # Only show the results from the main process.
+            pprint(info)
+            print(f"\n\nMean TFLOPS/HPU: {info['Model Mean TFLOPS'] / world_size:.1f} TFLOPS\n\n")
